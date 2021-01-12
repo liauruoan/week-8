85a131a1902bbb116d515dd26d7694943d5448d2
LiauRuoAn <LiauRuoAn@gmail.com>
2021-01-13 00:10:32 +0800

    Update 2021-01-12T16:10:25.900Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -4,4 +4,15 @@ console.log('Hello!');
 $(document).ready(function () {
   console.log('HesSchool Hello!');
 });
+"use strict";
+
+var mySwiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  direction: 'horizontal',
+  speed: 2000,
+  autoplay: {
+    delay: 2000
+  },
+  loop: true
+});
 //# sourceMappingURL=all.js.map
