33da2e4bcb7ab5e8ae25ec0e43a64a746038f152
LiauRuoAn <LiauRuoAn@gmail.com>
2021-02-27 13:00:57 +0800

    Update 2021-02-27T05:00:51.418Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,4 +1,21 @@
 "use strict";
+
+$(document).ready(function () {
+  $('.top').click(function (event) {
+    event.preventDefault();
+    $('html,body').animate({
+      scrollTop: 0
+    }, 1000);
+  });
+}); // 頁面往下滾動後 TOP 才會出現
+
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 400) {
+    $('.top').fadeIn();
+  } else {
+    $('.top').stop().fadeOut();
+  }
+});
 "use strict";
 
 var mySwiper = new Swiper('.swiper-container', {
