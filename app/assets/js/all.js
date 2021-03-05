64f5f0197faaa723229efc1318875e964e3362ea
LiauRuoAn <LiauRuoAn@gmail.com>
2021-03-05 12:17:27 +0800

    '修改檔案'

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,5 @@
+// GOTOP
+
 $(document).ready(function () {
 
   $('.top').click(function(event) { 
@@ -6,5 +8,21 @@ $(document).ready(function () {
       scrollTop: 0
     }, 1000);
     
+  });
+});
+
+
+// swiper
+
+$(document).ready(function () {
+  //initialize swiper when document ready
+  var mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    speed: 2000,
+    autoplay: {
+      delay: 2000,
+    },
+    loop: true,
   });
 });
